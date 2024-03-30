f742c49d426505746ef8187f3416ac9f82250b0a
Vera Kasianenko <112972833+VeraKasianenko@users.noreply.github.com>
2024-03-30 14:33:04 +0300

    remove dir loop

--- squeezer.py
+++ squeezer.py
@@ -9,11 +9,8 @@ def compress_images(folder_path):
 
     for filename in os.listdir(folder_path):
         filepath = os.path.join(folder_path, filename)
-        if os.path.isdir(filepath):
-            if filename.lower() == "all":
-                continue
-            compress_images(filepath)
-        elif os.path.isfile(filepath) and any(filename.lower().endswith(ext) for ext in ('.jpg', '.jpeg', '.png')):
+
+        if os.path.isfile(filepath) and any(filename.lower().endswith(ext) for ext in ('.jpg', '.jpeg', '.png')):
             file_size_mb = os.path.getsize(filepath) / (1024 * 1024)
             if file_size_mb > 3:
                 try:
